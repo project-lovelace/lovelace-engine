d49135468e1bfd70fdc02ced468916fc66d787a7
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2017-05-19 15:42:55 -0400

    Process return code, runtime, and memory usage is now reported but with low precision.

--- engine/api.py
+++ engine/api.py
@@ -9,7 +9,7 @@ import util
 
 # Config applies to all other loggers
 logging.basicConfig(format='[%(asctime)s] %(name)s:%(levelname)s: %(message)s', level=logging.DEBUG)
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class SubmitResource(object):
@@ -31,7 +31,7 @@ class SubmitResource(object):
         if code:
             decoded_code = str(base64.b64decode(code), 'utf-8')
             code_filename = util.write_str_to_file(decoded_code)
-            log.debug("User code saved in: %s", code_filename)
+            logger.debug("User code saved in: %s", code_filename)
         else:
             raise falcon.HTTPError(falcon.HTTP_400, 'Invalid JSON.', 'No code provided.')
 
@@ -51,31 +51,36 @@ class SubmitResource(object):
 
         for tc in problem.test_cases:
             input_str = tc.input_str()
-            log.info("Test case %d/%d (%s).", n, num_cases, tc.test_type.test_name)
+            logger.info("Test case %d/%d (%s).", n, num_cases, tc.test_type.test_name)
             tc_panel_title = 'Test case {:d}/{:d} ({}): '.format(n, num_cases, tc.test_type.test_name)
 
-            log.debug("Input string:")
-            log.debug("%s", input_str)
+            logger.debug("Input string:")
+            logger.debug("%s", input_str)
 
-            user_answer = PythonRunner().run(code_filename, input_str)
+            user_answer, process_info = PythonRunner().run(code_filename, input_str)
 
-            log.debug("Output string:")
-            log.debug("%s", user_answer)
+            logger.debug("Output string:")
+            logger.debug("%s", user_answer)
+
+            p_info_str = 'Return code {:d}, utime: {:f} s, stime: {:f} s, maxrss: {:d} kB.<br>'\
+                .format(process_info['return_code'], process_info['utime'], process_info['stime'],
+                        process_info['maxrss'])
 
             success = problem.verify_user_solution(input_str, user_answer)
 
             if success:
                 successes[n-1] = True
-                log.info("Test case passed!")
+                logger.info("Test case passed!")
                 tc_panel_title += 'passed!'
             else:
                 successes[n-1] = False
-                log.info("Test case failed.")
+                logger.info("Test case failed.")
                 tc_panel_title += 'failed.'
 
             test_case_details += '<div class="panel panel-default">'\
                                  + '<div class="panel-heading" style="font-size: medium;">' + tc_panel_title + '</div>'\
                                  + '<div class="panel-body" style="font-family: monospace; font-size: medium;">'\
+                                 + p_info_str\
                                  + 'Input:<br>' + input_str + '<br><br>'\
                                  + 'Output:<br>' + user_answer + '</div>'\
                                  + '</div>'
@@ -86,7 +91,7 @@ class SubmitResource(object):
             if success:
                 passes += 1
 
-        log.info("Passed %d/%d test cases.", passes, num_cases)
+        logger.info("Passed %d/%d test cases.", passes, num_cases)
         details_html += 'Passed {:d}/{:d} test cases.<br>'.format(passes, num_cases)
         details_html += test_case_details + '</p>'
 
@@ -102,7 +107,7 @@ class SubmitResource(object):
         resp.body = json.dumps(resp_dict)
 
         util.delete_file(code_filename)
-        log.debug("User code file deleted: %s", code_filename)
+        logger.debug("User code file deleted: %s", code_filename)
 
 
 app = falcon.API()
--- engine/runners/python_runner.py
+++ engine/runners/python_runner.py
@@ -1,6 +1,4 @@
-# python_runner.py
-
-import abc
+import resource  # Note: this is a UNIX-specific module.
 from subprocess import run, Popen, PIPE, CalledProcessError, TimeoutExpired
 
 import util
@@ -16,8 +14,10 @@ class PythonRunner(AbstractRunner):
 
         try:
             timeout_seconds = 1
+            r0 = resource.getrusage(resource.RUSAGE_CHILDREN)
             process = Popen(command, stdin=PIPE, stdout=PIPE)
-            stdout = process.communicate(input=bytes(input_str, encoding='utf-8'), timeout=timeout_seconds)[0]
+            stdout, stderr = process.communicate(input=bytes(input_str, encoding='utf-8'), timeout=timeout_seconds)
+            r = resource.getrusage(resource.RUSAGE_CHILDREN)
         except CalledProcessError as ex:
             logger.critical("%s", ex)
             logger.critical("Program returned non-zero code %s", ex.returncode)
@@ -30,4 +30,16 @@ class PythonRunner(AbstractRunner):
             logger.critical("Program output: %s", ex.stdout)
 
         output_str = stdout.decode('utf-8').strip()
-        return output_str
+
+        p_info = {
+            'return_code': process.returncode,
+            'utime': r.ru_utime - r0.ru_utime,
+            'stime': r.ru_stime - r0.ru_stime,
+            'maxrss': r.ru_maxrss
+        }
+
+        logger.debug("Finished running user code. Return code %d.", p_info['return_code'])
+        logger.debug("utime: %f, stime: %f", p_info['utime'], p_info['stime'])
+        logger.debug("maxrss: %d kB", p_info['maxrss'])
+
+        return output_str, p_info
