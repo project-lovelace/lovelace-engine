0a7291a42ae5a1d1de62604236d8e9718f23b72d
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2018-07-28 18:23:39 -0400

    Trying to figure out how to run submitted code...
    Might have to serialize the arguments to a JSON/pickle file then load them in the subprocess and then call the function with the arguments... =/

--- new-minimal-working-example/mwe.py
+++ new-minimal-working-example/mwe.py
@@ -7,18 +7,16 @@ log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loggin
 logging.config.fileConfig(log_file_path)
 logger = logging.getLogger(__name__)
 
+submission_filename = 'user_submitted_code.py'
+submission_module = 'user_submitted_code'
+
+problem_name = 'i04_dna_transcription'
+problem_module = 'i04_dna_transcription'  # Could be 'problems.i04_dna_transcription'
+function_name = 'dna_complement'
+
 if __name__ == '__main__':
-    submission_filename = 'user_submitted_code.py'
-    submission_module = 'user_submitted_code'
-
-    problem_name = 'i04_dna_transcription'
-    problem_module = 'i04_dna_transcription'  # Could be 'problems.i04_dna_transcription'
-
     logger.info('Importing problem module: {:s}'.format(problem_module))
     problem = importlib.import_module(problem_module)
-    
-    logger.info('Importing submission: {:s}'.format(submission_module))
-    submission = importlib.import_module(submission_module)
 
     logger.info("Generating test cases...")
     test_case_type_enum = problem.TEST_CASE_TYPE_ENUM
@@ -33,4 +31,16 @@ if __name__ == '__main__':
     num_cases = len(test_cases)
     test_case_details = []  # List of dicts each containing the details of a particular test case.
 
-    logger.info(psutil.cpu_times())
+    logger.info('Importing submission: {:s}'.format(submission_module))
+    submission = importlib.import_module(submission_module)
+
+    for i, tc in enumerate(test_cases):
+
+        input_args = tc.input_args()
+        user_output = submission.dna_complement(input_args)
+
+        logger.info("Test case %d/%d (%s).", i+1, num_cases, tc.test_type.test_name)
+        logger.debug("Input arguments:")
+        logger.debug("%s", input_args)
+        logger.debug("User output:")
+        logger.debug("%s", user_output)
