e6b1e21c73eeef6153666c58cf96dddba0b7096f
basimr <basim.ramadhan@gmail.com>
2018-12-30 11:04:29 -0500

    Changed unit test to test all the problems with the engine

--- engine/test_api.py
+++ engine/test_api.py
@@ -1,17 +1,25 @@
+import base64
 import json
+import os
 import unittest
 
 import requests
 
 class TestApi(unittest.TestCase):
-    def test_rocket_science(self):
-        payload_dict = {
-            'problem': 'rocket-science',
-            'language': 'python3',
-            'code': 'aW1wb3J0IG1hdGgKCnZfZSA9IDI1NTAgICMgcm9ja2V0IGV4aGF1c3QgdmVsb2NpdHkgW20vc10KTSA9IDI1MDAwMCAgIyByb2NrZXQgZHJ5IG1hc3MgW2tnXQoKZGVmIHJvY2tldF9mdWVsKHYpOgogICAgcmV0dXJuIE0gKiAobWF0aC5leHAodiAvIHZfZSkgLSAxKQ==',
-        }
-        payload_json = json.dumps(payload_dict)
-        resp = requests.post('http://localhost:14714/submit', data=payload_json)
-        resp_data = json.loads(resp.text)
-        self.assertTrue(resp_data['success'])
+    def test_all_problems(self):
+        for solution_filename in os.listdir('../solutions/'):
+            problem_name, extension = solution_filename.split(sep='.')
+            language = {'py': 'python3', 'jl': 'julia'}.get(extension)
+            with open('../solutions/' + solution_filename, 'r') as solution_file:
+                code = solution_file.read()
+            code_b64 = base64.b64encode(code.encode('utf-8')).decode('utf-8')
+            payload_dict = {
+                'problem': problem_name,
+                'language': language,
+                'code': code_b64,
+            }
+            payload_json = json.dumps(payload_dict)
+            resp = requests.post('http://localhost:14714/submit', data=payload_json)
+            resp_data = json.loads(resp.text)
+            self.assertTrue(resp_data['success'], 'Failed when submitting good solution for {}\n{}'.format(problem_name, json.dumps(resp_data, indent=4)))
 
