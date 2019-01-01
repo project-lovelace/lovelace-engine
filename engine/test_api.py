f81bc9e6659236285f1f6269aeaf1ee116e06f58
basimr <basim.ramadhan@gmail.com>
2018-12-31 20:52:10 -0500

    Fixed tests to open solutions files from language-specific folders

--- engine/test_api.py
+++ engine/test_api.py
@@ -8,14 +8,16 @@ import requests
 
 class TestApi(unittest.TestCase):
     solutions_dir = '../solutions/'
+    python_solutions_dir = os.path.join(solutions_dir, 'python3')
+    javascript_solutions_dir = os.path.join(solutions_dir, 'javascript')
+    julia_solutions_dir = os.path.join(solutions_dir, 'julia')
 
-    def submit_solution(self, file_name):
-        solution_file_path = os.path.join(self.solutions_dir, file_name)
-        with open(solution_file_path, 'r') as solution_file:
+    def submit_solution(self, file_path):
+        with open(file_path, 'r') as solution_file:
             code = solution_file.read()
         code_b64 = base64.b64encode(code.encode('utf-8')).decode('utf-8')
 
-        problem_name, extension = file_name.split(sep='.')
+        problem_name, extension = os.path.basename(file_path).split(sep='.')
         language = {'py': 'python3', 'jl': 'julia', 'js': 'javascript'}.get(extension)
 
         payload_dict = {
@@ -30,16 +32,16 @@ class TestApi(unittest.TestCase):
 
         return response_data
 
-    def test_all_problems_python_success(self):
-        for file_name in os.listdir(self.solutions_dir):
-            if file_name.split('.')[1] == 'js':
-                pass
-            print('Submitting {}!'.format(file_name))
-            result = self.submit_solution(file_name)
-            self.assertTrue(result['success'], 'Failed. Engine output:\n{}'.format(json.dumps(result, indent=4)))
+    # def test_all_problems_python_success(self):
+    #     for file_name in os.listdir(self.python_solutions_dir):
+    #         file_path = os.path.join(self.python_solutions_dir, file_name)
+    #         print('Submitting {}!'.format(file_path))
+    #         result = self.submit_solution(file_path)
+    #         self.assertTrue(result['success'], 'Failed. Engine output:\n{}'.format(json.dumps(result, indent=4)))
 
     def test_all_problems_javascript_success(self):
-        file_name = 'rocket-science.js'
-        print('Submitting {}!'.format(file_name))
-        result = self.submit_solution(file_name)
-        self.assertTrue(result['success'], 'Failed. Engine output:\n{}'.format(json.dumps(result, indent=4)))
+        for file_name in os.listdir(self.javascript_solutions_dir):
+            file_path = os.path.join(self.javascript_solutions_dir, file_name)
+            print('Submitting {}!'.format(file_path))
+            result = self.submit_solution(file_path)
+            self.assertTrue(result['success'], 'Failed. Engine output:\n{}'.format(json.dumps(result, indent=4)))
