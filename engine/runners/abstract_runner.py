ceda336a95e2bb3da911dc9d90f156d70426bcfc
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-07-18 12:07:04 -0400

    Nuke old runners

--- engine/runners/abstract_runner.py
+++
@@ -1,8 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AbstractRunner(metaclass=ABCMeta):
-
-    @abstractmethod
-    def run(self, container_name, filename, function_name, input_str):
-        """Execute the given file using input_str as input through stdin and return the program's output."""
