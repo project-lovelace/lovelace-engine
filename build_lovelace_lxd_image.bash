fe01f982b4ee4b3afba70496f53577f0b8769128
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-04 14:13:58 -0400

    Fully working build script

--- build_lovelace_lxd_image.bash
+++ build_lovelace_lxd_image.bash
@@ -23,10 +23,8 @@ set -x  # Print a trace of simple commands.
 
 # images:archlinux does not have internet connection out-of-the-box so we're using ubuntu for now.
 # If archlinux was working, it would make installing the latest versions of packages much easier.
-echo "Downloading Ubuntu image..."
 lxc image copy images:ubuntu/disco local: --alias ubuntu
 
-echo "Launching Linux container..."
 lxc launch ubuntu lovelace-build
 
 # I think this is needed because if I immediately start apt installing, the internet connection
@@ -35,36 +33,30 @@ echo "Sleeping for 30 seconds until image fully spins up..."
 sleep 30
 
 # Ubuntu disco dingo already comes with Python 3.7.3 so no need to install Python.
-echo "Installing Python packages..."
 lxc exec lovelace-build -- sudo apt update
 lxc exec lovelace-build -- sudo apt install -y python3-pip
 lxc exec lovelace-build -- sudo pip3 install numpy scipy bitstring
 
-echo "Installing Julia..."
-lxc exec lovelace-build -- sudo apt install -y wget
-lxc exec lovelace-build -- wget https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz
-lxc exec lovelace-build -- tar xvf julia-1.1.0-linux-x86_64.tar.gz
-lxc exec lovelace-build -- mv ~/julia-1.1.0 ~/julia && echo 'PATH=$PATH:~/julia/bin' >> ~/.bashrc && source ~/.bashrc
+lxc exec lovelace-build -- sudo apt install -y julia
+# lxc exec lovelace-build -- sudo apt install -y wget
+# lxc exec lovelace-build -- wget https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz
+# lxc exec lovelace-build -- tar xf julia-1.1.0-linux-x86_64.tar.gz
+# lxc exec lovelace-build -- mv /root/julia-1.1.0 /root/julia
+# lxc exec lovelace-build -- sh -c "echo 'PATH=$PATH:~/julia/bin' >> /root/.bashrc"
+# lxc exec lovelace-build -- bash -c "source /root/.bashrc"
 
-echo "Installing latest version of node.js..."
 # lxc exec lovelace-build -- sudo apt install -y curl software-properties-common
 # lxc exec lovelace-build -- curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 lxc exec lovelace-build -- sudo apt install -y nodejs
 
-echo "Installing gcc..."
 lxc exec lovelace-build -- sudo apt install -y gcc
 
-echo "python3 --version"
 lxc exec lovelace-build -- python3 --version
-echo "node --version"
 lxc exec lovelace-build -- node --version
-echo "julia --version"
 lxc exec lovelace-build -- julia --version
-echo "cc --version"
 lxc exec lovelace-build -- cc --version
 
-echo "Publishing lovelace-image..."
 lxc publish lovelace-build --force --alias lovelace-image
 
-echo "Stopping lovelace-build container..."
+lxc image delete ubuntu
 lxc delete --force lovelace-build
